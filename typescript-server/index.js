713f28f73a4a5da871b5f08ed01c8833b9badc01
Devere-Anthony <devereaweaver@icloud.com>
2024-02-10 20:23:43 -0500

    working on typescript server

---
+++ typescript-server/index.js
@@ -0,0 +1,5 @@
+/* This Express.js server extends the example server we've been working with in 
+ * the previous exercises. It does so by using TypeScript instead of regular JS
+ * to create some custom types. 
+ */
+
